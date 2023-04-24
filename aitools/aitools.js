1f55fb8a790973ccdf30cc8fb49e2006d7bb39d2
linyunliu <linyunliu574@gmail.com>
2023-04-23 21:22:29 -0700

    update Linyun final

--- aitools/aitools.js
+++ aitools/aitools.js
@@ -11,12 +11,33 @@ function goToQuillBot(){
     window.open("https://quillbot.com/")
 }
 
-function addApiKey() {
-    let response = prompt("Enter API Key Here:")
-    if (response.toString().trim() !== "") {
-        alert(response.toString())
-    }
+// for getting the data from the textbox
+function extract(){
+    let text = document.getElementById("message")
+    return text.value.toString().trim();
 }
 
+async function send() {
+    let DATA = extract();
+    console.log("\n[Front]: sending message..")
+    try {
+        const response = await fetch('http://localhost:1689/process', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: DATA.toString()
+        });
+        let data = await response.text();
+        // display the response on the webpage
+        console.log("[Front]: response received")
 
+        // Here to display data on webpage
+        let text = document.getElementById("content")
+        text.value = text.value.concat(data.trim().concat("\n\n"));
+
+    } catch (error) {
+        console.error(error);
+    }
+}
 
--- aitools/server/index.js
+++ aitools/server/index.js
@@ -1,15 +1,66 @@
+// Specify program requirements
 const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
 
-const OPENAI_API_KEY= "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";
-
-const configuration = new Configuration({apiKey: OPENAI_API_KEY});
+// configure openai key and create openai client
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 const openai = new OpenAIApi(configuration);
 
-async function generateText(){
-    const completion = await openai.createCompletion({
-        model: "gpt-3.5-turbo",
-        messages: "tell me about Node.js",
-    });
-    console.log(completion.data.choices[0].text);
+messages = []
+messages.push({ role: "user", content: "Hello!" })
+let completion_text = "";
+
+
+async function chat(MESSAGE) {
+    // add user message to the array of messaging history
+    messages.push({role: "user", content: MESSAGE.toString()})
+
+    // now try to get the response back from openAI
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+        });
+        completion_text = completion.data.choices[0].message.content;
+        // now we have to add AI response as well to the history, this how ChatGPT works
+        messages.push({role: "assistant", content: completion_text.toString()})
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+    }
 }
-generateText()
+
+
+
+// Start the server and respond to the request
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 1689;
+const bodyParser = require('body-parser');
+app.use(bodyParser.text()); // parse plain text body
+
+const corsOptions = {
+    origin: 'http://localhost:63342',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.post('/process', cors(corsOptions), (req, res) =>{
+    const data = req.body;
+    console.log("[Back]: message received, waiting for AI response")
+    // once we have received the data we just need to send to chat to process
+    chat(data).then(() => {
+        res.send(completion_text.toString())
+        console.log(completion_text)
+        console.log("[Back] response sent")
+    });
+});
+app.listen(port, () =>{
+    console.log('[Back]: Sever has started...');
+});
